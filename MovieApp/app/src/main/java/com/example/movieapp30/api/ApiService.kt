fdf0ec9b755883122d57b99c715b38ff96f5a099
AkramAlisher <akramch228@gmail.com>
2020-04-28 20:24:47 +0600

    all coroutines added

--- MovieApp/app/src/main/java/com/example/movieapp30/api/ApiService.kt
+++ MovieApp/app/src/main/java/com/example/movieapp30/api/ApiService.kt
@@ -7,6 +7,7 @@ import com.google.gson.JsonObject
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Call
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.*
@@ -50,67 +51,68 @@ object RetrofitService{
 }
 
 interface PostApi{
-    @GET("authentication/token/new")
-    fun createRequestToken(
-        @Query("api_key") apiKey: String
-    ): Call<GetRequestTokenResponse>
-
-    @POST("authentication/token/validate_with_login")
-    fun CreateSessionWithLogin(
-        @Query("api_key") apiKey: String?,
-        @Body body: JsonObject
-    ): Call<GetRequestTokenResponse>
-
-    @POST("authentication/session/new")
-    fun CreateSession(
-        @Query("api_key") apiKey: String?,
-        @Body body: JsonObject
-    ): Call<CreateSessionResponse>
 
     @GET("movie/popular")
-    fun getPopularMoviesList(
+    suspend fun getPopularMoviesList(
         @Query("api_key") apiKey: String?,
         @Query("page") page: Int,
         @Query("language") lang: String?
-    ): Call<MovieResponse>
+    ): Response<MovieResponse>
 
-    @GET("account")
-    @Headers(
-        "Content-Type: application/json;charset=utf-8"
-    )
-    fun getAccountDetails(
-        @Query("api_key") apiKey: String?,
-        @Query("session_id") session_id: String?
-    ): Call<AccountDetailsResponse>
-
-    @POST("account/{account_id}/favorite")
-    fun markAsFavourite(
-        @Path(value = "account_id", encoded = true) account_id: Int,
-        @Query("api_key") apiKey: String?,
-        @Query("session_id") session_id: String?,
-        @Body body: JsonObject
-    ): Call<JsonObject>
-
-    @GET("movie/{movie_id}")
-    fun getMovie(
-        @Path(value = "movie_id", encoded = true) movie_id: Int,
-        @Query("api_key") apiKey: String?,
-        @Query("language") lang: String?
-    ): Call<Movie>
 
     @GET("account/{account_id}/favorite/movies")
-    fun getFavouritesMoviesList(
+    suspend fun getFavouritesMoviesList(
         @Path(value = "account_id", encoded = true) account_id: Int,
         @Query("api_key") apiKey: String,
         @Query("session_id") session_id: String,
         @Query("page") page: Int,
         @Query("language") lang: String?
-    ): Call<MovieResponse>
+    ): Response<MovieResponse>
+
+    @GET("authentication/token/new")
+    suspend fun createRequestToken(
+        @Query("api_key") apiKey: String
+    ): Response<GetRequestTokenResponse>
+
+    @POST("authentication/token/validate_with_login")
+    suspend fun createSessionWithLogin(
+        @Query("api_key") apiKey: String?,
+        @Body body: JsonObject
+    ): Response<GetRequestTokenResponse>
+
+    @POST("authentication/session/new")
+    suspend fun createSession(
+        @Query("api_key") apiKey: String?,
+        @Body body: JsonObject
+    ): Response<CreateSessionResponse>
+
+    @GET("account")
+    @Headers(
+        "Content-Type: application/json;charset=utf-8"
+    )
+    suspend fun getAccountDetails(
+        @Query("api_key") apiKey: String?,
+        @Query("session_id") session_id: String?
+    ): Response<AccountDetailsResponse>
+
+    @POST("account/{account_id}/favorite")
+    suspend fun markAsFavourite(
+        @Path(value = "account_id", encoded = true) account_id: Int,
+        @Query("api_key") apiKey: String?,
+        @Query("session_id") session_id: String?,
+        @Body body: JsonObject
+    ): Response<JsonObject>
+
+    @GET("movie/{movie_id}")
+    suspend fun getMovie(
+        @Path(value = "movie_id", encoded = true) movie_id: Int,
+        @Query("api_key") apiKey: String?,
+        @Query("language") lang: String?
+    ): Response<Movie>
 
     @HTTP(method = "DELETE", path = "authentication/session", hasBody = true)
-    fun deleteSession(
+    suspend fun deleteSession(
         @Query("api_key") apiKey: String,
         @Body body: JsonObject
-    ): Call<JsonObject>
-
+    ): Response<JsonObject>
 }
